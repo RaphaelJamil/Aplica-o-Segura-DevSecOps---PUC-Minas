e582e375a45b9004bcae8ec1568776e357b5ce96
RaphaelJamil <raphinhajamil627@gmail.com>
2026-05-14 20:47:13 -0300

    implementando pipeline inicial com falhas propositais

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Um servidor simples em Node.js
 const express = require('express');
 const app = express();
 
@@ -6,8 +5,7 @@ app.get('/', (req, res) => {
   res.send('Aplicação Segura DevSecOps - PUC Minas');
 });
 
-// SIMULAÇÃO DE VULNERABILIDADE (Para o Semgrep detectar)
-// Nunca use eval() com entrada de usuário!
+
 function maliciosa(input) {
     eval(input); 
 }
